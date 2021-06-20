6588df58d1776daf3575a080dfea2849d7fd2a08
Krystian Grzelak <krystian.grzelak@o2.pl>
2021-06-20 10:38:45 +0200

    added config option to html plugin

--- webpack/common.js
+++ webpack/common.js
@@ -20,6 +20,7 @@ module.exports = {
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: 'public/template.html',
+            inject: 'body',
         }),
     ],
 };
